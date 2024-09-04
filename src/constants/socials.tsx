f523d46f45bb80ec51cd9121ddd215d8b1ed6b11
Dipak Giri <dipakgiri1200@gmail.com>
2024-09-04 15:02:18 +0545

    initial project added

---
+++ src/constants/socials.tsx
@@ -0,0 +1,30 @@
+import {
+  IconBrandLinkedin,
+  IconBrandGithub,
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconX,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://github.com/dipaktech",
+    label: "Github",
+    icon: IconBrandGithub,
+  },
+  {
+    href: "https://x.com/dipakgirii",
+    label: "Twitter",
+    icon: IconX,
+  },
+  {
+    href: "https://linkedin.com/in/dpakgiri",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+  {
+    href: "https://www.youtube.com/@dipakgiri6072",
+    label: "YouTube",
+    icon: IconBrandYoutube,
+  },
+];
